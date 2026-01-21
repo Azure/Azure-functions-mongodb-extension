b3edcfc3c95c1c8295500e3b1801c0cb098ea654
Xing Fan <xingfan@microsoft.com>
2026-01-21 16:34:12 +0800

    more

--- Sample/Sample.cs
+++ Sample/Sample.cs
@@ -14,8 +14,6 @@ namespace Sample
 {
     public static class Sample
     {
-        #region Entra ID Authentication Sample
-
         [FunctionName("EntraIdAuthSample")]
         public static void EntraIdAuthRun(
             [TimerTrigger("*/30 * * * * *")] TimerInfo myTimer,
@@ -50,10 +48,6 @@ namespace Sample
             }
         }
 
-        #endregion
-
-        #region Client Binding Samples
-
         [FunctionName("ClientBindingSample")]
         public static void ClientBindingRun(
             [TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
@@ -70,37 +64,6 @@ namespace Sample
                 log.LogInformation(d.ToString());
             }
         }
-        
-        [FunctionName("ClientBindingWithEntraIdSample")]
-        public static void ClientBindingWithEntraIdRun(
-            [TimerTrigger("*/30 * * * * *")] TimerInfo myTimer,
-            [CosmosDBMongo(
-                ConnectionStringSetting = "EntraIdConnection",
-                AuthMethod = AuthMethod.MicrosoftEntraID,
-                TenantId = "%TenantId%")] IMongoClient client,
-            ILogger log)
-        {
-            log.LogInformation($"Entra ID Auth Sample executed at: {DateTime.Now}");
-
-            try
-            {
-                // List databases to verify connection
-                var databases = client.ListDatabaseNames().ToList();
-                log.LogInformation($"Connected via Entra ID. Found {databases.Count} databases:");
-                foreach (var db in databases)
-                {
-                    log.LogInformation($"  - {db}");
-                }
-            }
-            catch (Exception ex)
-            {
-                log.LogError(ex, "Failed to connect with Entra ID authentication");
-            }
-        }
-
-        #endregion
-
-        #region Output Binding Samples
 
         [FunctionName("OutputBindingSample")]
         public static async Task OutputBindingRun(
@@ -120,30 +83,6 @@ namespace Sample
             await collector.AddAsync(item);
         }
 
-        [FunctionName("OutputBindingWithEntraIdSample")]
-        public static async Task OutputBindingWithEntraIdRun(
-            [TimerTrigger("*/30 * * * * *")] TimerInfo myTimer,
-            [CosmosDBMongo("%DatabaseName%", "%CollectionName%",
-                ConnectionStringSetting = "EntraIdConnection",
-                AuthMethod = AuthMethod.MicrosoftEntraID,
-                TenantId = "%TenantId%")] IAsyncCollector<TestClass> collector,
-            ILogger log)
-        {
-            log.LogInformation($"Output binding with Entra ID executed at: {DateTime.Now}");
-
-            TestClass item = new TestClass()
-            {
-                id = Guid.NewGuid().ToString(),
-                SomeData = "data inserted via Entra ID auth"
-            };
-
-            await collector.AddAsync(item);
-        }
-
-        #endregion
-
-        #region Input Binding Samples
-
         [FunctionName("InputBindingSample")]
         public static void InputBindingRun(
             [TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
@@ -160,28 +99,6 @@ namespace Sample
             }
         }
 
-        [FunctionName("InputBindingWithEntraIdSample")]
-        public static void InputBindingWithEntraIdRun(
-            [TimerTrigger("*/30 * * * * *")] TimerInfo myTimer,
-            [CosmosDBMongo("%DatabaseName%", "%CollectionName%",
-                ConnectionStringSetting = "EntraIdConnection",
-                AuthMethod = AuthMethod.MicrosoftEntraID,
-                TenantId = "%TenantId%",
-                QueryString = "%QueryString%")] List<BsonDocument> docs,
-            ILogger log)
-        {
-            log.LogInformation($"Input binding with Entra ID executed at: {DateTime.Now}");
-
-            foreach (var doc in docs)
-            {
-                log.LogInformation(doc.ToString());
-            }
-        }
-
-        #endregion
-
-        #region Trigger Samples
-
         [FunctionName("TriggerSample")]
         public static void TriggerRun(
             [CosmosDBMongoTrigger("TestDatabase", "TestCollection",
@@ -193,25 +110,6 @@ namespace Sample
             log.LogInformation($"Change detected at: {DateTime.Now}");
             log.LogInformation(doc.FullDocument.ToString());
         }
-
-        /// <summary>
-        /// Sample: Change stream trigger with Entra ID authentication
-        /// </summary>
-        [FunctionName("TriggerWithEntraIdSample")]
-        public static void TriggerWithEntraIdRun(
-            [CosmosDBMongoTrigger("TestDatabase", "TestCollection",
-                ConnectionStringSetting = "EntraIdConnection",
-                AuthMethod = AuthMethod.MicrosoftEntraID,
-                TenantId = "%TenantId%",
-                LeaseDatabaseName = "TestDatabase",
-                LeaseCollectionName = "leases")] ChangeStreamDocument<BsonDocument> doc,
-            ILogger log)
-        {
-            log.LogInformation($"Change detected via Entra ID at: {DateTime.Now}");
-            log.LogInformation(doc.FullDocument.ToString());
-        }
-
-        #endregion
     }
 
     public class TestClass
